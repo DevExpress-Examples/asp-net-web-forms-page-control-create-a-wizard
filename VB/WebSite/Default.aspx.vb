9fd7956c424c2b09c9eb001f2b457c6a1c4edf9f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.Web.ASPxEditors
+
+Imports System
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub showTabs_Init(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not IsCallback) AndAlso (Not IsPostBack) Then
+			Dim cb As ASPxCheckBox = CType(sender, ASPxCheckBox)
+			cb.Checked = pageControl.ShowTabs
+		End If
+	End Sub
+	Protected Sub showTabs_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs)
+		Dim cb As ASPxCheckBox = CType(sender, ASPxCheckBox)
+		pageControl.ShowTabs = cb.Checked
+		popupControl.ShowOnPageLoad = False
+	End Sub
+
+End Class
