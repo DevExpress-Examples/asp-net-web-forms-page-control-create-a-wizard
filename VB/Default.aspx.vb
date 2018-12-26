396a462443e944e1c0a1d89798584349a35d198d
DevExpressExampleBot <fakeEmail>
2018-12-26 19:55:58 +0300

    Source auto update [skip ci]

--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -4,15 +4,8 @@ Imports DevExpress.Web
 Partial Public Class _Default
     Inherits System.Web.UI.Page
 
-    Protected Sub showTabs_Init(ByVal sender As Object, ByVal e As EventArgs)
-        If (Not IsCallback) AndAlso (Not IsPostBack) Then
-            Dim cb As ASPxCheckBox = DirectCast(sender, ASPxCheckBox)
-            cb.Checked = pageControl.ShowTabs
-        End If
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
     End Sub
-    Protected Sub showTabs_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs)
-        Dim cb As ASPxCheckBox = DirectCast(sender, ASPxCheckBox)
-        pageControl.ShowTabs = cb.Checked
-        popupControl.ShowOnPageLoad = False
-    End Sub
+
 End Class
